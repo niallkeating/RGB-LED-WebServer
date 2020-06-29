7b0986807289c6eb9559a705e9b55b6d70087eb7
niallkeating <niallkeating44@gmail.com>
2020-06-29 15:23:23 +0100

    Converted buttons to sliders for each RGB element

--- src/main.cpp
+++ src/main.cpp
@@ -5,173 +5,161 @@
 
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncTCP.h>
+
+//Refactoring Arduino code to be proper C++
+String outputState(void);
 
 // Replace with your network credentials
 const char* ssid     = "BTHub6-M9XC";
 const char* password = "ALMn94YgiEmb";
 
-// Set web server port number to 80
-WiFiServer server(80);
+const char* PARAM_INPUT_1 = "RValue";
+const char* PARAM_INPUT_2 = "GValue";
+const char* PARAM_INPUT_3 = "BValue";
 
-// Variable to store the HTTP request
-String header;
+const int RedGPIO = 5;
+const int GreenGPIO = 4;
+const int BlueGPIO = 14;
 
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output4State = "off";
-String output14State = "off";
+String RedSliderValue = "100";
+String GreenSliderValue = "100";
+String BlueSliderValue = "100";
 
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
-const int output14 = 14;
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>RGB LED Web Server</title>
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 2.4rem;}
+    p {font-size: 2.2rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+    input:checked+.slider {background-color: #2196F3}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+    .slider2 { -webkit-appearance: none; margin: 14px; width: 300px; height: 20px; background: #ccc;
+      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
+    .slider2::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 30px; height: 30px; background: #2f4468; cursor: pointer;}
+    .slider2::-moz-range-thumb { width: 30px; height: 30px; background: #2f4468; cursor: pointer; } 
+  </style>
+</head>
+<body>
+  <h2>LED Web Server</h2>
+  <p>Red: <span id="RedValue">%REDVALUE%</span></p>
+  <p><input type="range" onchange="updateRedSlider(this)" id="RedSlider" min="0" max="100" value="%REDVALUE%" step="1" class="slider2"></p>
+  <p>Green: <span id="GreenValue">%GREENVALUE%</span></p>
+  <p><input type="range" onchange="updateGreenSlider(this)" id="GreenSlider" min="0" max="100" value="%GREENVALUE%" step="1" class="slider2"></p>
+  <p>Blue: <span id="BlueValue">%BLUEVALUE%</span></p>
+  <p><input type="range" onchange="updateBlueSlider(this)" id="BlueSlider" min="0" max="100" value="%BLUEVALUE%" step="1" class="slider2"></p>
+  
+<script>
+function updateRedSlider(element) {
+  var sliderValue = document.getElementById("RedSlider").value;
+  document.getElementById("RedValue").innerHTML = sliderValue;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?RValue="+sliderValue, true);
+  xhr.send();
+}
+function updateGreenSlider(element) {
+  var sliderValue = document.getElementById("GreenSlider").value;
+  document.getElementById("GreenValue").innerHTML = sliderValue;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?GValue="+sliderValue, true);
+  xhr.send();
+}
+function updateBlueSlider(element) {
+  var sliderValue = document.getElementById("BlueSlider").value;
+  document.getElementById("BlueValue").innerHTML = sliderValue;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?BValue="+sliderValue, true);
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
 
-void setup() {
-  Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(output4, OUTPUT);
-  pinMode(output14, OUTPUT);
-
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(output4, LOW);
-  digitalWrite(output14, LOW);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "REDVALUE"){
+    return RedSliderValue;
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  else if(var == "GREENVALUE"){
+    return GreenSliderValue;
+  }
+  else if(var == "BLUEVALUE"){
+    return BlueSliderValue;
+  }
+  return String();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, LOW);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /14/on") >= 0) {
-              Serial.println("GPIO 14 on");
-              output14State = "on";
-              digitalWrite(output14, LOW);
-            } else if (header.indexOf("GET /14/off") >= 0) {
-              Serial.println("GPIO 14 off");
-              output14State = "off";
-              digitalWrite(output14, HIGH);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
+  pinMode(RedGPIO, OUTPUT);
+  analogWrite(RedGPIO, 0);
 
-            // Display current state, and ON/OFF buttons for GPIO 14  
-            client.println("<p>GPIO 14 - State " + output14State + "</p>");
-            // If the output14State is off, it displays the ON button       
-            if (output14State=="off") {
-              client.println("<p><a href=\"/14/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/14/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+  pinMode(GreenGPIO, OUTPUT);
+  analogWrite(GreenGPIO, 0);
+
+  pinMode(BlueGPIO, OUTPUT);
+  analogWrite(BlueGPIO, 0);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  
+  // Send a GET request to <ESP_IP>/slider?value=<inputMessage>
+  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      RedSliderValue = inputMessage;
+      analogWrite(RedGPIO, static_cast<int>((1.0 - RedSliderValue.toInt()/100.0)*1023));
+    }
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      GreenSliderValue = inputMessage;
+      analogWrite(GreenGPIO, static_cast<int>((1.0 - GreenSliderValue.toInt()/100.0)*1023));
+    }
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage = request->getParam(PARAM_INPUT_3)->value();
+      BlueSliderValue = inputMessage;
+      analogWrite(BlueGPIO, static_cast<int>((1.0 - BlueSliderValue.toInt()/100.0)*1023));
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+  
+  // Start server
+  server.begin();
+}
+  
+void loop() {
+
 }
